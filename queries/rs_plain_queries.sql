3facf5f4071c8b2fbd5771fc88cd040ac43d2038
charles de Courcel (Ubuntu 20.04) <charles.decourcel@the-ica.com>
2020-09-08 00:44:25 +0200

    Benchmark schema and queries

---
+++ queries/rs_plain_queries.sql
@@ -0,0 +1,64 @@
+/* Q1 */
+
+SELECT str0, val
+FROM (
+        select str0, sum(value) as val, row_number() OVER (PARTITION BY str0 ORDER BY val) as number
+        from factTable
+        group by str0, position
+    )
+WHERE number = 50;
+
+/* Q2 */
+
+SELECT str0, str1, int10, int11, dttime10, dttime11, val
+FROM (
+        SELECT str0, str1, int10, int11, dttime10, dttime11,
+                sum(value) as val,
+                row_number()
+                OVER (PARTITION BY str0, str1, int10, int11, dttime10, dttime11 ORDER BY sum(value))            as number
+        from factTable
+        group by str0, str1, int10, int11, dttime10, dttime11, position
+    )
+WHERE number = 50;
+
+
+/* Q3 */
+
+SELECT str0, str1, str2, str3,
+    int10,
+    int11,
+    int12,
+    int13,
+    dttime10,
+    dttime11,
+    dttime12,
+    dttime13,
+    val
+FROM (
+        SELECT str0,
+            str1,
+            str2,
+            str3,
+            int10,
+            int11,
+            int12,
+            int13,
+            dttime10,
+            dttime11,
+            dttime12,
+            dttime13,
+            sum(value)   as val,
+            row_number()
+            OVER (PARTITION BY str0, str1, str2, str3, int10, int11, int12, int13, dttime10, dttime11, dttime12, dttime13 ORDER BY val) as number
+        from factTable
+        group by str0, str1, str2, str3, int10, int11, int12, int13, dttime10, dttime11, dttime12, dttime13, position
+    )
+WHERE number = 50;
+
+
+/* Q4 */
+
+select str0, sum(value) as val, position
+from factTable
+WHERE str1 = 'KzORBHFRuFFOQm'
+group by str0, position
